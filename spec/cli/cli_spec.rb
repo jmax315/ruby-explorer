26850f51e0cad06924876441b0dd756c6732263c
John Maxwell <jmax@jmaxhome.com>
2020-04-07 21:21:13 -0400

    Rethink no-arguments behavior.

--- spec/cli/cli_spec.rb
+++ spec/cli/cli_spec.rb
@@ -1,10 +1,24 @@
 describe "The CLI" do
-  before do
-    $the_app= nil
-    load("bin/ruby-explorer")
+  describe "with no arguments" do
+    before do
+      $the_app= nil
+      load("bin/ruby-explorer")
+    end
+
+    it "does not create the app" do
+      expect($the_app).to be_nil
+    end
   end
 
-  it "creates the app" do
-    expect($the_app).not_to be_nil
+  describe "with a single non-option argument" do
+    before do
+      $the_app= nil
+      load("bin/ruby-explorer")
+    end
+
+    it "creates the app" do
+      skip
+      expect($the_app).not_to be_nil
+    end
   end
 end
