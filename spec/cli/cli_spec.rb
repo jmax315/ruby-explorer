7a1b33fe7da1556690b561d9fe06318485ccc83a
John Maxwell <jmax@jmaxhome.com>
2020-04-08 12:07:56 -0400

    Finished command-line wrapper.

--- spec/cli/cli_spec.rb
+++ spec/cli/cli_spec.rb
@@ -1,50 +1,96 @@
-def set_argv(new_value)
-  Object.__send__(:remove_const, :ARGV)
-  Object.const_set(:ARGV, new_value)
-end
+require './src/ruby_explorer'
 
 describe "The CLI" do
-  let(:argv) { [] }
-
-  before do
-    $the_app= nil
-    set_argv(argv)
-    load("bin/ruby-explorer")
+  def set_argv(new_value)
+    Object.__send__(:remove_const, :ARGV)
+    Object.const_set(:ARGV, new_value)
   end
 
-  describe "with no arguments" do
-    let(:argv) { [] }
+  def invoke_cli_with(argv)
+    $the_app= nil
+    @error_exit= nil
+    @stderr_output= nil
+    @saved_stderr= $stderr
+    $stderr= StringIO.new
+
+    set_argv(argv)
+    begin
+      load("bin/ruby-explorer")
+    rescue SystemExit => e
+      @error_exit= !e.success?
+    end
+    @stderr_output= $stderr.string
+    $stderr= @saved_stderr
+  end
+
+  def self.expect_cli_to_fail_with(argv)
+    before do
+      invoke_cli_with(argv)
+    end
 
     it "does not create the app" do
       expect($the_app).to be_nil
     end
 
-    it "prints a usage message to stderr"
+    it "prints a usage message to stderr" do
+      expect(@stderr_output).to match(/usage:/)
+    end
+
+    it "exits with an error status" do
+      expect(@error_exit).to be(true)
+    end
   end
 
-  describe "with a single argument" do
-    let(:argv) { ["not-an-option"] }
+  def self.expect_cli_to_succeed_with(argv)
+    before do
+      invoke_cli_with(argv)
+    end
 
     it "creates the app" do
       expect($the_app).not_to be_nil
     end
+
+    it "does not print anything to stderr" do
+      expect(@stderr_output).to eq("")
+    end
+
+    it "exits with an success status" do
+      expect(@error_exit).to be(false)
+    end
+
+    yield if block_given?
+  end
+
+
+  before do
+    allow_any_instance_of(RubyExplorer).to receive(:run)
+  end
+
+  describe "with no arguments" do
+    expect_cli_to_fail_with []
   end
 
   describe "with more than one argument" do
-    let(:argv) { ["one-option", "another-option"] }
-
-    it "does not create the app" do
-      expect($the_app).to be_nil
-    end
+    expect_cli_to_fail_with ["one-option", "another-option"]
   end
 
   describe "with a short help option" do
-    let(:argv) { ["-h"] }
+    expect_cli_to_fail_with ["-h"]
+  end
 
-    it "does not create the app" do
-      expect($the_app).to be_nil
+  describe "with a long help option" do
+    expect_cli_to_fail_with ["--help"]
+  end
+
+  describe "with a single argument" do
+    expect_cli_to_succeed_with ["not-an-option"] do
+      it "sets the app's target directory" do
+        expect($the_app.target_directory).to eq("not-an-option")
+      end
+
+      it "calls the app's run method" do
+        expect($the_app).to have_received(:run)
+      end
     end
-
-    it "prints a usage message to stderr"
   end
 end
--- src/ruby_explorer.rb
+++ src/ruby_explorer.rb
@@ -1,2 +1,6 @@
 class RubyExplorer
+  attr_accessor :target_directory
+
+  def run
+  end
 end
