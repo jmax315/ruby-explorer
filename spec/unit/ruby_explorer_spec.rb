8f4ed90de9a42c6e7180c9d7cbb0035ff47cb5a9
John Maxwell <jmax@jmaxhome.com>
2020-04-17 21:39:21 -0400

    Chdir to target directory

--- spec/unit/ruby_explorer_spec.rb
+++ spec/unit/ruby_explorer_spec.rb
@@ -2,17 +2,21 @@ require './src/ruby_explorer'
 
 describe "RubyExplorer#run" do
   let(:app) { RubyExplorer.new }
+  let(:target_directory) {"target_directory"}
 
   before do
+    allow(Dir).to receive(:chdir)
     allow(Process).to receive(:spawn)
+
+    app.target_directory= target_directory
     app.run
   end
 
   it "spawns the probe inserter" do
+    expect(Dir).to have_received(:chdir).with(target_directory).ordered
     expect(Process).to have_received(:spawn).with("/bin/bash",
                                                   "-l",
                                                   "-c",
-                                                  "ruby -r probe bin/rails")
-
+                                                  "ruby -r probe bin/rails").ordered
   end
 end
--- src/ruby_explorer.rb
+++ src/ruby_explorer.rb
@@ -2,6 +2,7 @@ class RubyExplorer
   attr_accessor :target_directory
 
   def run
+    Dir.chdir(target_directory)
     Process.spawn("/bin/bash", "-l", "-c", "ruby -r probe bin/rails")
   end
 end
