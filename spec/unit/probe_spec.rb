c926657ff9d8f159f4badcb495b58e4c8d9ca9e0
John Maxwell <jmax@jmaxhome.com>
2020-04-21 12:44:04 -0400

    Wrap module method.

--- spec/unit/probe_spec.rb
+++ spec/unit/probe_spec.rb
@@ -58,7 +58,7 @@ describe "Probe" do
   end
 
   describe "#wrap_class_method" do
-    class WrapTarget
+    class WrapClassMethodTarget
       @@a_class_method_called= false
 
       def self.a_method
@@ -68,12 +68,12 @@ describe "Probe" do
 
     before do
       @got_called_back= false
-      the_probe.wrap_class_method(WrapTarget, :a_method) do |original_method|
+      the_probe.wrap_class_method(WrapClassMethodTarget, :a_method) do |original_method|
         @got_called_back= true
         original_method.call
       end
 
-      WrapTarget.a_method
+      WrapClassMethodTarget.a_method
     end
 
     it "calls the wrap block back" do
@@ -81,7 +81,45 @@ describe "Probe" do
     end
 
     it "calls the original method" do
-      expect(WrapTarget.class_variable_get(:@@a_class_method_called)).to be(true)
+      expect(WrapClassMethodTarget.class_variable_get(:@@a_class_method_called)).to be(true)
+    end
+  end
+
+  describe "#wrap on a module method" do
+    module WrapModule
+      def a_module_method
+        @a_module_method_called= true
+      end
+    end
+
+    class WrapModuleMethodTarget
+      include WrapModule
+
+      attr_accessor :a_module_method_called
+
+      def initialize
+        @a_module_method_called= false
+      end
+    end
+
+    let(:wrap_target) { WrapModuleMethodTarget.new }
+
+    before do
+      @got_called_back= false
+      the_probe.wrap(WrapModule, :a_module_method) do |original_method|
+        @got_called_back= true
+        original_method.call
+      end
+
+      wrap_target.a_module_method
+    end
+
+    it "calls the wrap block back" do
+      expect(@got_called_back).to be(true)
+    end
+
+    it "calls the original method" do
+      expect(wrap_target.a_module_method_called).to be(true)
     end
   end
 end
